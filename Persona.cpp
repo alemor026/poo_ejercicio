da841f2ca34abe462f7b67700129ff9bb4afaa46
alemor026 <smoralesb6@miumg.edu.gt>
2021-02-16 22:19:58 -0600

    primer_commit

---
+++ Persona.cpp
@@ -0,0 +1,44 @@
+//Persona = nombre, apellido, direccion, telefono, fecha de nacimiento, dpi, etc...
+//Cliente = nit, etc... 
+#include <iostream>
+
+using namespace std;
+
+class Persona{
+
+	// atributos
+
+	protected : string nombres,apellidos,direccion;
+
+				int telefono;
+
+	// costructor
+
+	protected : 
+
+		Persona(){
+
+		}
+
+		Persona(string nom,string ape,string dir,int tel){
+
+			nombres = nom;
+
+			apellidos = ape;
+
+			direccion = dir;
+
+			telefono = tel;
+
+		}
+
+	
+
+	// metodos
+
+	void mostrar();
+
+	
+
+};
+
